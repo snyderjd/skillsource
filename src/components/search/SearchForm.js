54dd1eb78ef1865ee3a9b54ef927f4849473565b
snyder <snyderjosephd@yahoo.com>
2019-09-17 16:18:08 -0500

    All search components on one page with dropdown to select search domain

--- src/components/search/SearchForm.js
+++ src/components/search/SearchForm.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import SearchDataManager from './SearchDataManager';
 import { Button } from 'reactstrap';
 import ResultCard from './ResultCard';
+import VideoResult from '../videos/VideoResult';
+import WebResult from '../web/WebResult';
+import VideoDataManager from '../videos/VideoDataManager';
+import WebDataManager from '../web/WebDataManager';
 import './Search.css';
 
 class SearchForm extends Component {
     state = {
         searchInput: "",
-        results: []
+        results: [],
+        searchDomain: "",
+        videoResults: [],
+        webResults: []
     }
 
     handleFieldChange = (event) => {
@@ -19,11 +26,35 @@ class SearchForm extends Component {
     // Call function to search skills with the input from state and save results to state
     executeSearch = (event) => {
         event.preventDefault();
-        SearchDataManager.searchSkills(this.state.searchInput).then(skills => {
-            console.log(skills);
-            skills.sort((a, b) => b.timesCopied - a.timesCopied)
-            this.setState({ results: skills })
-        })
+
+        if (this.state.searchDomain === "skills") {
+            SearchDataManager.searchSkills(this.state.searchInput).then(skills => {
+                skills.sort((a, b) => b.timesCopied - a.timesCopied)
+                this.setState({ 
+                    results: skills,
+                    videoResults: [],
+                    webResults: []
+                })
+            })
+        } else if (this.state.searchDomain === "videos") {
+            VideoDataManager.getVideos(this.state.searchInput).then(videos => {
+                this.setState({ 
+                    videoResults: videos.items,
+                    results: [],
+                    webResults: []
+                })
+            })
+        } else if (this.state.searchDomain === "web") {
+            WebDataManager.getWebResults(this.state.searchInput).then(results => {
+                this.setState({ 
+                    webResults: results.items,
+                    videoResults: [],
+                    results: []
+                })
+            })
+        } else {
+            window.alert("Please select what you would like to search for.")
+        }
 
     }
 
@@ -32,7 +63,18 @@ class SearchForm extends Component {
             <React.Fragment>
                 <div className="SearchForm-container">
                     <div className="SearchForm-input-container">
-                        <label>Search for Items</label>
+                        <label htmlFor="type">Search For</label>
+                        <select
+                            id="searchDomain"
+                            value={this.state.searchDomain}
+                            className="VideoModal-input"
+                            onChange={this.handleFieldChange}
+                        >
+                            <option>Select</option>
+                            <option key="skills" value="skills">Skills</option>
+                            <option key="videos" value="videos">Videos</option>
+                            <option key="web" value="web">Web Content</option>
+                        </select>
                         <input
                             className="searchInput"
                             type="text"
@@ -43,13 +85,31 @@ class SearchForm extends Component {
                         <Button onClick={this.executeSearch} color="success">Search</Button>
                     </div>
                     <div className="resultContainer">
-                            {this.state.results.map(result =>
-                                <ResultCard
-                                    key={result.id}
-                                    result={result}
-                                    {...this.props}
-                                />
-                            )}
+                        {this.state.results.map(result =>
+                            <ResultCard
+                                key={result.id}
+                                result={result}
+                                {...this.props}
+                            />
+                        )}
+                    </div>
+                    <div className="resultContainer">
+                        {this.state.videoResults.map(video =>
+                            <VideoResult
+                                key={video.id.videoId}
+                                video={video}
+                                {...this.props}
+                            />
+                        )}
+                    </div>
+                    <div className="resultContainer">
+                        {this.state.webResults.map(result =>
+                            <WebResult
+                                key={result.cacheId}
+                                result={result}
+                                {...this.props}
+                            />
+                        )}
                     </div>
                 </div>
             </React.Fragment>
